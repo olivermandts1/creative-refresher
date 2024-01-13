33387a24d279da879d998359b4cd9fc6b5d8add7
olivermandts1 <104032123+olivermandts1@users.noreply.github.com>
2024-01-13 00:47:27 +0000

    test

--- examples/pages/Dataframe.py
+++ examples/pages/Dataframe.py
@@ -1,12 +1,14 @@
 import streamlit as st   
+import pandas as pd
+import numpy as np
    
    
-    # Editable DataFrame Section using st.data_editor
-    st.subheader("📥 Clipboard")
-    st.caption("This is a demo of the `st.data_editor`.")
+# Editable DataFrame Section using st.data_editor
+st.subheader("📥 Clipboard")
+st.caption("This is a demo of the `st.data_editor`.")
 
-    # Create an empty grid
-    empty_grid = pd.DataFrame(np.zeros((20, 4))).replace(0, "").astype(str)
+# Create an empty grid
+empty_grid = pd.DataFrame(np.zeros((20, 4))).replace(0, "").astype(str)
 
-    # Use st.data_editor for an editable DataFrame
-    df = st.data_editor(empty_grid, use_container_width=True, height=600)
+# Use st.data_editor for an editable DataFrame
+df = st.data_editor(empty_grid, use_container_width=True, height=600)
