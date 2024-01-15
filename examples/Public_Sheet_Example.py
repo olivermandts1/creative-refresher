0151c764a4fb383a750c3f05b2b6c82574ff22a1
olivermandts1 <104032123+olivermandts1@users.noreply.github.com>
2024-01-15 23:02:11 +0000

    test

--- examples/Public_Sheet_Example.py
+++ examples/Public_Sheet_Example.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.subheader("📗 Google Sheets st.connection using Public URLs")
 
-url = "https://docs.google.com/spreadsheets/d/1BkD450mJ4MJA7xuMi0MFrb79_bXsS_9HaSLTykKRccE/edit?usp=sharing"
+url = "https://docs.google.com/spreadsheets/d/1BkD450mJ4MJA7xuMi0MFrb79_bXsS_9HaSLTykKRccE/edit#gid=1465532381"
 
 st.write("#### 1. Read public Google Worksheet as Pandas")
 
