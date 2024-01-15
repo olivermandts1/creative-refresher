827c635f65e40bf7b846a5f05c26ba06464dfe34
olivermandts1 <104032123+olivermandts1@users.noreply.github.com>
2024-01-15 23:23:20 +0000

    updating labels

--- examples/Public_Sheet_Example.py
+++ examples/Public_Sheet_Example.py
@@ -1,28 +1,27 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-st.subheader("📗 Google Sheets Connection using Public URLs")
+st.subheader("📗 Refresh FB Creatives From Your Plutus Spreadsheet")
 
 url = "https://docs.google.com/spreadsheets/d/141YaOszXibklI2qqRiyGdox3mpyCioFK5eJMtD78iJE/edit#gid=962857946"
 
-st.write("#### 1. Read public Google Worksheet as Pandas")
+st.write("#### 1. Retrieved Plutus Creatives")
 
-with st.echo():
-    # Create a connection using Streamlit's experimental connection feature
-    conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+# Create a connection using Streamlit's experimental connection feature
+conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-    # Read data from the Google Sheet
-    df = conn.read(spreadsheet=url)
-    desired_range = df.iloc[99:124, 0:2]  # Rows 100-124 and columns A-B (0-indexed)
+# Read data from the Google Sheet
+df = conn.read(spreadsheet=url)
+desired_range = df.iloc[99:124, 0:2]  # Rows 100-124 and columns A-B (0-indexed)
 
-    # Hardcoding specific values in column A
-    desired_range.iloc[0:5, 0] = 'Headlines'      # Rows 100-104
-    desired_range.iloc[6:11, 0] = 'Primary Text'  # Rows 106-110
-    desired_range.iloc[12:17, 0] = 'Description'  # Rows 112-116
-    desired_range.iloc[19:24, 0] = 'Forcekeys'    # Rows 119-123
-    
-    # Replace NaN values with an empty string
-    desired_range.fillna('', inplace=True)
+# Hardcoding specific values in column A
+desired_range.iloc[0:5, 0] = 'Headlines'      # Rows 100-104
+desired_range.iloc[6:11, 0] = 'Primary Text'  # Rows 106-110
+desired_range.iloc[12:17, 0] = 'Description'  # Rows 112-116
+desired_range.iloc[19:24, 0] = 'Forcekeys'    # Rows 119-123
+
+# Replace NaN values with an empty string
+desired_range.fillna('', inplace=True)
 
 # Use st.markdown with HTML and CSS to enable text wrapping
 st.markdown("""
