6c14a99ac99a3b1d114fc8387e14c3c2f53cf51c
olivermandts1 <104032123+olivermandts1@users.noreply.github.com>
2024-01-15 23:14:46 +0000

    testing better formatting

--- examples/Public_Sheet_Example.py
+++ examples/Public_Sheet_Example.py
@@ -1,21 +1,28 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
-st.subheader("📗 Google Sheets st.connection using Public URLs")
+st.subheader("📗 Google Sheets Connection using Public URLs")
 
 url = "https://docs.google.com/spreadsheets/d/141YaOszXibklI2qqRiyGdox3mpyCioFK5eJMtD78iJE/edit#gid=962857946"
 
 st.write("#### 1. Read public Google Worksheet as Pandas")
 
 with st.echo():
-    import streamlit as st
-
-    from streamlit_gsheets import GSheetsConnection
-
+    # Create a connection using Streamlit's experimental connection feature
     conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
+    # Read data from the Google Sheet
     df = conn.read(spreadsheet=url)
-    desired_range = df.iloc[99:124, 0:2]  # Rows 101-124 and columns A-B (0-indexed)
-    st.dataframe(desired_range)
+    desired_range = df.iloc[99:124, 1:2]  # Rows 100-124 and columns A-B (0-indexed)
+
+    # Hardcoding specific values in column A
+    desired_range.iloc[0:5, 0] = 'Headlines'      # Rows 100-104
+    desired_range.iloc[6:11, 0] = 'Primary Text'  # Rows 106-110
+    desired_range.iloc[12:17, 0] = 'Description'  # Rows 112-116
+    desired_range.iloc[19:24, 0] = 'Forcekeys'    # Rows 119-123
+
+    # Display the DataFrame with expanded width
+    st.dataframe(desired_range, width=700)
 
 st.write("#### 2. Query public Google Worksheet using SQL")
 st.info(
