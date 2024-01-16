1b5a10036922b9a6a08737e4028fb396eb062153
olivermandts1 <104032123+olivermandts1@users.noreply.github.com>
2024-01-16 18:20:39 +0000

    asdf

--- examples/pages/prompt_engineering_form_test.py
+++ examples/pages/prompt_engineering_form_test.py
@@ -11,7 +11,7 @@ conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
 # Fetch existing data
 worksheet_name = "https://docs.google.com/spreadsheets/d/1tqm7G0yzckwSCKXdPcGcWNH6y5nMj68rhpMQZlcO2wU"
-existing_data = conn.read(worksheet=worksheet_name, usecols=[0], ttl=5)
+existing_data = conn.read(spreadsheet=worksheet_name, usecols=[0], ttl=5)
 existing_data = existing_data.dropna(how="all")
 
 # Data Submission Form
